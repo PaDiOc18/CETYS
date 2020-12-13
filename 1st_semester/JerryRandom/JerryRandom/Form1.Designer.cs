0fc494e6a3205df2cafc2f34f3604a7dfb569fb0
Pablo Díaz <50026864+PaDiOc18@users.noreply.github.com>
2020-12-13 12:42:42 -0800

    Add files via upload

---
+++ 1st_semester/JerryRandom/JerryRandom/Form1.Designer.cs
@@ -0,0 +1,288 @@
+﻿namespace JerryRandom
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.label3 = new System.Windows.Forms.Label();
+            this.timer3 = new System.Windows.Forms.Timer(this.components);
+            this.label6 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.timer4 = new System.Windows.Forms.Timer(this.components);
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(630, 479);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(123, 90);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "RANDOM!";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(204, 43);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "label1";
+            this.label1.Visible = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(208, 74);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(152, 140);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Visible = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "4"});
+            this.comboBox1.Location = new System.Drawing.Point(661, 412);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(92, 21);
+            this.comboBox1.TabIndex = 5;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(422, 74);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(2);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(152, 140);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 6;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Visible = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Location = new System.Drawing.Point(208, 276);
+            this.pictureBox3.Margin = new System.Windows.Forms.Padding(2);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(152, 140);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 7;
+            this.pictureBox3.TabStop = false;
+            this.pictureBox3.Visible = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Location = new System.Drawing.Point(422, 276);
+            this.pictureBox4.Margin = new System.Windows.Forms.Padding(2);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(152, 140);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox4.TabIndex = 8;
+            this.pictureBox4.TabStop = false;
+            this.pictureBox4.Visible = false;
+            // 
+            // timer2
+            // 
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(417, 43);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(57, 20);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "label3";
+            this.label3.Visible = false;
+            // 
+            // timer3
+            // 
+            this.timer3.Tick += new System.EventHandler(this.timer3_Tick);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(202, 243);
+            this.label6.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(57, 20);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "label6";
+            this.label6.Visible = false;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(415, 243);
+            this.label8.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(57, 20);
+            this.label8.TabIndex = 14;
+            this.label8.Text = "label8";
+            this.label8.Visible = false;
+            // 
+            // timer4
+            // 
+            this.timer4.Interval = 1000;
+            this.timer4.Tick += new System.EventHandler(this.timer4_Tick);
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.CheckOnClick = true;
+            this.checkedListBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Items.AddRange(new object[] {
+            "Adolfo",
+            "Alec",
+            "Alejandro",
+            "Carlos",
+            "David",
+            "Diego",
+            "el 08",
+            "Fausto",
+            "Angel",
+            "Isaac Reyes",
+            "Isaac Sanchez",
+            "Isaac",
+            "Juampy",
+            "Kevin Huerta",
+            "Kevin",
+            "Lola",
+            "Manolo",
+            "Mejia",
+            "Miguel",
+            "Mizael",
+            "Nestor",
+            "Oscar",
+            "Pablo",
+            "Rubs",
+            "Sando",
+            "Silvia",
+            "Smith",
+            "Xavier"});
+            this.checkedListBox1.Location = new System.Drawing.Point(18, 24);
+            this.checkedListBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(156, 526);
+            this.checkedListBox1.TabIndex = 1;
+            this.checkedListBox1.SelectedIndexChanged += new System.EventHandler(this.checkedListBox1_SelectedIndexChanged);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(463, 479);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(143, 90);
+            this.button2.TabIndex = 15;
+            this.button2.Text = "LIMPIAR";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(790, 571);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.checkedListBox1);
+            this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Timer timer3;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Timer timer4;
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ 1st_semester/JerryRandom/JerryRandom/Form1.cs
@@ -0,0 +1,237 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Media;
+
+namespace JerryRandom
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            comboBox1.SelectedIndex=0;
+        }
+        private void checkedListBox1_SelectedIndexChanged(object sender, EventArgs e)
+        { }
+        Random NumAleatorio = new Random();
+        string[] Imagenes = new string[28] { "Adolfo.jpg", "Alec.jpg", "Alejandro.jpg", "Carlos.jpg", "David.jpg", "Diego.jpg", "el 08.jpg", "Fausto.jpg", "Grauyi.jpg", "Isaac Reyes.jpg", "Isaac Sanchez.jpg", "Isaac.jpg", "Juampy.jpg", "Kevin Huerta.jpg", "Kevin.jpg", "Lola.jpg", "Manolo.jpg", "Mejia.jpg", "Miguel.jpg", "Mizael.jpg", "Nestor.jpg", "Oscar.jpg", "Pablo.jpg", "Rubs.jpg", "Sando.jpg", "Silvia.jpg", "Smith.jpg", "Xavier.jpg" };
+        string[] ArregloNombres = new string[28] { "Adolfo", "Alec", "Alejandro", "Carlos", "David", "Diego", "el 08", "Fausto", "Angel", "Isaac Reyes", "Isaac Sanchez", "Isaac", "Juampy", "Kevin Huerta", "Kevin", "Lola", "Manolo", "Mejia", "Miguel", "Mizael", "Nestor", "Oscar", "Pablo", "Rubs", "Sando", "Silvia", "Smith", "Xavier" };
+        public int NumRandomVerificado()
+        {
+            int ValorRegresar = 0;
+            if (Anticiclo() == 28)
+            {
+                timer1.Enabled = false;
+                timer2.Enabled = false;
+                timer3.Enabled = false;
+            }
+            else
+            {
+            hola:
+                int Num = NumAleatorio.Next(0, 28);
+                if (checkedListBox1.GetItemChecked(Num) == false)
+                {
+                    ValorRegresar = Num;
+                }
+                else
+                {
+                    goto hola;
+                }
+            }
+            return ValorRegresar;
+        }
+        int C1 = 0;
+        public int Anticiclo()
+        {
+            C1 = 0;
+            foreach (var item in checkedListBox1.CheckedItems)
+            {
+                C1++;
+            }
+            return C1;
+        }
+        System.Media.SoundPlayer player1 = new System.Media.SoundPlayer("SM.wav");
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedIndex == 0)
+            {
+                player1.Play();
+                pictureBox1.Width = 400; pictureBox1.Height = 400;
+                pictureBox1.Visible = true; pictureBox2.Visible = false; pictureBox3.Visible = false; pictureBox4.Visible = false;
+                label1.Visible = true;  label3.Visible = false;  label6.Visible = false;
+                label8.Visible = false; 
+                timer1.Enabled = true;
+                if (Anticiclo() == 28)
+                {
+                    player1.Stop();
+                    timer1.Enabled = false;
+                    MessageBox.Show("Desmarca las casillas Por favor");
+
+                }
+                else { timer1.Enabled = true; }
+            }
+            else if (comboBox1.SelectedIndex == 1)
+            {
+                player1.Play();
+                pictureBox1.Width = 152; pictureBox1.Height = 140;
+                pictureBox1.Visible = true; pictureBox2.Visible = true; pictureBox3.Visible = false; pictureBox4.Visible = false;
+                label1.Visible = true;  label3.Visible = true;  label6.Visible = false;
+                label8.Visible = false; 
+                timer2.Enabled = true;               
+                if (Anticiclo() == 28)
+                {
+                    player1.Stop();
+                    timer2.Enabled = false;
+                    MessageBox.Show("Desmarca las casillas Por favor");
+                }
+                else if(Anticiclo() >= 27)
+                {
+                    player1.Stop();
+                    timer2.Enabled = false;
+                    MessageBox.Show("Desmarca mínimo 2 casillas Por favor");
+                }
+                    else { timer2.Enabled = true; }
+            }
+            else if(comboBox1.SelectedIndex == 2)
+            {
+                player1.Play();
+                pictureBox1.Visible = true; pictureBox2.Visible = true; pictureBox3.Visible = true; pictureBox4.Visible = true;
+                label1.Visible = true;  label3.Visible = true;  label6.Visible = true;
+                label8.Visible = true; 
+                if (Anticiclo() == 28)
+                {
+                    player1.Stop();
+                    timer3.Enabled = false;
+                    MessageBox.Show("Desmarca las casillas Por favor");
+                }
+                else if (Anticiclo() >= 25)
+                {
+                    player1.Stop();
+                    timer3.Enabled = false;
+                    MessageBox.Show("Desmarca mínimo 4 casillas Por favor");
+                }
+                    else { timer3.Enabled = true; }
+            }
+            else { MessageBox.Show("Selecciona un numero"); }
+        }
+        int DuraTimer = 0;
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            DuraTimer++;
+            int a = NumRandomVerificado();
+            pictureBox1.Image = Image.FromFile(Imagenes[a]);
+            label1.Text = ArregloNombres[a];
+            if (DuraTimer == 30)
+            {
+                player1.Stop();
+                timer1.Enabled = false;
+                DuraTimer = 0;
+                timer4.Enabled = true;
+            }
+        }
+        int DuraTimer2 = 0;
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            DuraTimer2++;
+            int a = NumRandomVerificado(); int b = NumRandomVerificado();
+            if (a == b)
+            {
+                while (b == a)
+                {
+                    if (Anticiclo() == 28)
+                    {
+                        timer2.Enabled = false;
+                        player1.Stop();
+                        break;
+                    }
+                    else
+                    { b = NumRandomVerificado(); }                    
+                }
+            }
+            pictureBox1.Image = Image.FromFile(Imagenes[a]); pictureBox2.Image = Image.FromFile(Imagenes[b]);
+            label1.Text = ArregloNombres[a]; 
+            label3.Text = ArregloNombres[b]; 
+            if (DuraTimer2 == 30)
+            {
+                timer2.Enabled = false;
+                DuraTimer2 = 0;
+                player1.Stop();
+            }
+        }
+        int DuraTimer3 = 0;
+        private void timer3_Tick(object sender, EventArgs e)
+        {
+            //Mete numeros en un arreglo y compararlos 
+            DuraTimer3++;
+            int a = NumRandomVerificado(); int b = NumRandomVerificado(); int c = NumRandomVerificado(); int d = NumRandomVerificado();
+            int[] CuatroNumeros = new int[4];
+            CuatroNumeros[0] = a; CuatroNumeros[1] = b; CuatroNumeros[2] =c; CuatroNumeros[3] = d;
+            if (DuraTimer3 == 30)
+            {
+                timer3.Enabled = false;
+                player1.Stop();
+                DuraTimer3 = 0;
+                CicloAntijeje:
+                for (int i = 0; i < CuatroNumeros.Length; i++)
+                {
+                    int conta = 0;
+                    for (int j = 0; j < CuatroNumeros.Length; j++)
+                    {
+                        if (CuatroNumeros[i] == CuatroNumeros[j])
+                        {
+                            conta = conta + 1;
+                        }
+                    }
+                    if (conta >1)
+                    {
+                        CuatroNumeros[i] = NumRandomVerificado();
+                        goto CicloAntijeje;
+                    }
+                }          
+            }
+            pictureBox1.Image = Image.FromFile(Imagenes[CuatroNumeros[0]]); pictureBox2.Image = Image.FromFile(Imagenes[CuatroNumeros[1]]);
+            pictureBox3.Image = Image.FromFile(Imagenes[CuatroNumeros[2]]); pictureBox4.Image = Image.FromFile(Imagenes[CuatroNumeros[3]]);
+            label1.Text = ArregloNombres[CuatroNumeros[0]]; 
+            label3.Text = ArregloNombres[CuatroNumeros[1]]; 
+            label6.Text = ArregloNombres[CuatroNumeros[2]]; 
+            label8.Text = ArregloNombres[CuatroNumeros[3]]; 
+        }
+        int Duratimer4;
+        private void timer4_Tick(object sender, EventArgs e)
+        {
+            Duratimer4++;
+            if (Duratimer4 == 4)
+            {
+                pictureBox1.Image = Image.FromFile("Surprise.jpg");
+                System.Media.SoundPlayer player2 = new System.Media.SoundPlayer("SurpriseM.wav");
+                player2.Play();
+            }
+            else if (Duratimer4 == 7)
+            {
+                int h = NumRandomVerificado();
+                pictureBox1.Image = Image.FromFile(Imagenes[h]);
+                label1.Text = ArregloNombres[h];
+                timer4.Enabled = false;
+                Duratimer4 = 0;
+            }
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < ArregloNombres.Length; i++)
+            {
+                checkedListBox1.SetItemChecked(i, false);
+            }
+        }
+    }
+}
